b2711e33c8031b2cb51ea621e9856c1b2b48e997
Felipecg10 <felipecazarini1@gmail.com>
2025-11-11 19:59:54 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-# setup.sh
-
 mkdir -p ~/.streamlit/
-
 echo "\
-[server]
-headless = true
-port = \$PORT
-enableCORS = false
-\n\
+[general]\n\
+email = \"felipecazarini1@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
+#
